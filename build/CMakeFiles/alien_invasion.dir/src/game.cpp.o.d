04104714a4598064b466be12703d018e130bafb5
M-Agoumi <agoumihunter@gmail.com>
2026-07-11 13:51:09 +0100

    remove non tracked files

--- build/CMakeFiles/alien_invasion.dir/src/game.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/alien_invasion.dir/src/game.cpp.o: \
- /home/magoumi/projects/alien-invasion-cpp/src/game.cpp \
- /usr/include/stdc-predef.h \
- /home/magoumi/projects/alien-invasion-cpp/src/game.h
--- src/game.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by agoum on 7/7/2026.
-//
-
-#include "game.h"
